6b41321261e23b792da5ad6483f04759f0d4f976
HyerimRyu <hyerim0812@naver.com>
2019-03-08 11:08:13 +0900

    20190308_02

--- app/src/main/java/kr/co/teada/whatiate/MainActivity.java
+++ app/src/main/java/kr/co/teada/whatiate/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.co.teada.whatiate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void clickBtnFab(View view) {
+        //갤러리 이동
+        openGallery();
 
 
+    }
+
+    public void openGallery(){
+        Intent photoPicintent=new Intent(Intent.ACTION_PICK);
+        photoPicintent.setType("image/*");
+        startActivityForResult(photoPicintent, 10);
+
+        saveData();
+    }
+
+    public void saveData(){
+
+    }
 
 }//end of  MainActivity
