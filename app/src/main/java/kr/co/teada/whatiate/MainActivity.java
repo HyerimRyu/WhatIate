25846704ee1b76f5771caca03494ad587fc31264
HyerimRyu <hyerim0812@naver.com>
2019-03-11 10:13:14 +0900

    20190308_02:firebase connect

--- app/src/main/java/kr/co/teada/whatiate/MainActivity.java
+++ app/src/main/java/kr/co/teada/whatiate/MainActivity.java
@@ -2,6 +2,7 @@ package kr.co.teada.whatiate;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,6 +12,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar=findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled( false );
 
         recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -58,18 +61,52 @@ public class MainActivity extends AppCompatActivity {
         navigationView=findViewById(R.id.navi);
         navigationView.setItemIconTintList(null);
 
+
+        navigationView.setNavigationItemSelectedListener( new navigationItemSelectedListener() );
+
+
         drawerToggle=new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.app_name, R.string.app_name);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
+
     }//end of onCreate
 
+    class navigationItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    public void clickBtnSearch(View view) {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            //Drawer 눌렀을 때 반응
+//            switch ( menuItem.getItemId() ){
+//
+//                case R.id.boomark:
+//                    Intent intent= new Intent( MainActivity.this, BookmarkActivity.class);
+//                    startActivity(intent);
+//                    break;
+//
+//                case R.id.setting:
+//                    intent= new Intent( MainActivity.this, SettingActivity.class);
+//                    startActivity(intent);
+//                    break;
+//
+//
+//            }
+            drawerLayout.closeDrawer( navigationView );
+            return false;
+
+        }
+    }
+
+
+
+
+        public void clickBtnSearch(View view) {
         //search Avtivity 이동
     }
 
 
+    // + 버튼 누르면, 갤러리에서 사진 고르고, add 액티비티 실행
     public void clickBtnFab(View view) {
         //갤러리 이동
         openGallery();
