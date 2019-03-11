a9a30f773e10723714b96062a36610c413343ec3
HyerimRyu <hyerim0812@naver.com>
2019-03-11 12:59:00 +0900

    xml welcome

---
+++ app/src/main/java/kr/co/teada/whatiate/IntroActivity.java
@@ -0,0 +1,31 @@
+package kr.co.teada.whatiate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Timer timer=new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        timer.schedule(task, 2000);
+    }
+
+    TimerTask task=new TimerTask() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(IntroActivity.this, WelcomActivity.class );
+            startActivity(intent);
+
+            finish();
+        }
+    };
+}
---
+++ app/src/main/java/kr/co/teada/whatiate/WelcomActivity.java
@@ -0,0 +1,26 @@
+package kr.co.teada.whatiate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+    }//end of onCreate
+
+    //프로필 사진 지정
+    public void clickImage(View view) {
+    }
+
+    //메인으로 이동
+    public void clickNext(View view) {
+    }
+
+
+
+
+}//end of WelcomActivity
